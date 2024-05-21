451f7489d44b2d4c1a39c03ccbf43ab7e5480096
Johan Uriarte <Johan.Uriarte@tranzact.net>
2024-05-20 19:33:39 -0500

    init

---
+++ src/components/menu/menu.jsx
@@ -0,0 +1,15 @@
+import './menu.css'
+
+function Menu() {
+  return (
+    <nav className='menu'>
+        <ul className='menu'>
+            <p>Home</p>
+            <p>About</p>
+            <p>Contact</p>
+        </ul>
+    </nav>
+  )
+}
+
+export default Menu
